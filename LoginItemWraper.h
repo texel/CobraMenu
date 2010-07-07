e4208aee389f71b6fe6774a3ddd315b597ec2141
Leigh Caplan <lcaplan@onehub.com>
2010-07-07 14:32:51 -0700

    Added a login items wrapper

---
+++ LoginItemWraper.h
@@ -0,0 +1,15 @@
+//
+//  LoginItemWraper.h
+//  CobraMenu
+//
+//  Created by Leigh Caplan on 7/7/10.
+//  Copyright 2010 Onehub, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface LoginItemWraper : NSObject {
+  +(void) addAppAsLoginItem
+  +(void) deleteAppFromLoginItems;
+@end
